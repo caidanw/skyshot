fcd5b0a2849bc8d4dd129d60e62e060ae5b4c8ad
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-03-21 19:56:14 -0700

    fix: clean up styles

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,23 +19,4 @@
 		align-items: center;
 		gap: 0.5rem;
 	}
-
-	.corner {
-		width: 2em;
-		height: 2em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
 </style>
