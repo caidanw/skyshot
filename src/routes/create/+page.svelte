eac846b4d56d43619cb34567e17ece3ec1e88017
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-03-21 19:58:44 -0700

    ref: update create page to use shadcn and work with at proto users

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import type { CardData } from "$lib";
   import TemplateCard from "$lib/components/TemplateCard.svelte";
+  import * as Avatar from "$lib/components/ui/avatar/index.js";
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card/index.js";
+  import { Input } from "$lib/components/ui/input";
+  import { Label } from "$lib/components/ui/label";
   import { AtpAgent } from "@atproto/api";
   import type { ProfileViewBasic } from "@atproto/api/dist/client/types/app/bsky/actor/defs";
 
@@ -8,18 +13,18 @@
     $type: "app.skyshot.card",
     photoUrl: "https://picsum.photos/id/64/300/400",
     metadata: {
-      title: "Card Title",
-      caption: "Card Caption",
+      title: "",
+      caption: "",
       createdAt: new Date().toISOString(),
-      location: "San Francisco, CA",
+      location: "",
     },
     subject: {
-      did: "did:plc:asdf",
-      handle: "Subject Name",
+      did: "",
+      handle: "",
     },
     photographer: {
-      did: "did:plc:asdf",
-      handle: "Photographer Name",
+      did: "",
+      handle: "",
     },
   });
 
@@ -45,6 +50,22 @@
       actors = response.data.actors;
     });
   }
+
+  // card.subject.handle = "a";
+  // findBskyUser();
+
+  const handleImageUpload = (event: Event) => {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        card.photoUrl = e.target?.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 </script>
 
 <div class="main-grid-container">
@@ -53,37 +74,65 @@
   </div>
 
   <div class="grid-item">
-    <div class="card-form-container">
-      <h2>Create New Card</h2>
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input
-          type="Subject Handle"
-          bind:value={card.subject.handle}
-          required
-          oninput={findBskyUser}
-        />
-      </div>
-      <div class="form-group">
-        <label for="photographer">Photographer</label>
-        <input
-          type="text"
-          id="photographer"
-          bind:value={card.photographer.handle}
-          required
-        />
-      </div>
-      <div class="form-group">
-        <label for="photo_url">Photo URL</label>
-        <input type="url" id="photo_url" bind:value={card.photoUrl} required />
-      </div>
-      <div class="form-actions">
-        <button type="button" class="cancel-button" onclick={history.back}>
-          Cancel
-        </button>
-        <button type="submit" class="submit-button">Create Card</button>
-      </div>
-    </div>
+    <Card.Root>
+      <Card.Header>
+        <Card.Title>Card Details</Card.Title>
+      </Card.Header>
+      <Card.Content class="space-y-4">
+        <div class="space-y-2 relative">
+          <Label for="subject-handle">Subject</Label>
+          <Input
+            id="subject-handle"
+            placeholder="Enter subject handle"
+            bind:value={card.subject.handle}
+            oninput={findBskyUser}
+          />
+          {#if actors.length > 0}
+            <Card.Root class="absolute overflow-scroll max-h-60 w-full">
+              {#each actors as actor (actor.did)}
+                <button
+                  class="flex items-center gap-2 p-2 hover:bg-accent cursor-pointer w-full text-left"
+                  onclick={() => {
+                    card.subject.did = actor.did;
+                    card.subject.handle = actor.handle;
+                    actors = [];
+                  }}
+                >
+                  <Avatar.Root>
+                    <Avatar.Image src={actor.avatar} alt={actor.handle} />
+                    <Avatar.Fallback>
+                      {actor.displayName?.substring(0, 2)}
+                    </Avatar.Fallback>
+                  </Avatar.Root>
+                  <p class="flex flex-col">
+                    <span>{actor.displayName}</span>
+                    <span class="text-sm text-muted-foreground">
+                      {actor.handle}
+                    </span>
+                  </p>
+                </button>
+              {/each}
+            </Card.Root>
+          {/if}
+        </div>
+        <div class="space-y-2">
+          <Label for="photographer-handle">Photographer</Label>
+          <Input
+            id="photographer-handle"
+            placeholder="Enter photographer handle"
+            bind:value={card.photographer.handle}
+          />
+        </div>
+        <div class="space-y-2">
+          <Label for="image">Photo</Label>
+          <Input id="image" type="file" onchange={handleImageUpload} />
+        </div>
+      </Card.Content>
+      <Card.Footer class="flex justify-between">
+        <Button variant="destructive" onclick={history.back}>Cancel</Button>
+        <Button type="submit" class="submit-button">Create Card</Button>
+      </Card.Footer>
+    </Card.Root>
   </div>
 </div>
 
