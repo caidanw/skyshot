6562d46cb59a0879511aa1ab13d41d5af0794e73
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-03-23 23:26:01 -0700

    enh: add threlte scene

---
+++ src/routes/(3d)/threlte/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Canvas } from "@threlte/core";
+	import Scene from "./Scene.svelte";
+</script>
+
+<!-- <div class="card-container flex-grow flex flex-col"> -->
+<Canvas>
+	<Scene />
+</Canvas>
+
+<!-- </div> -->
+
+<style>
+	.card-container {
+		width: 100%;
+		height: 100%;
+		cursor: pointer;
+	}
+	.card-container canvas {
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ src/routes/(3d)/threlte/Scene.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { T } from "@threlte/core";
+</script>
+
+<T.Mesh position.y={1}>
+	<T.BoxGeometry args={[2.5, 3.5, 1]} />
+	<T.MeshBasicMaterial color="hotpink" />
+</T.Mesh>
