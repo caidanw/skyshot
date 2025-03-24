d828f5c070c44a94a4c0c9d667bae956c0bfaef7
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-03-23 23:38:05 -0700

    enh: add perspective camera

--- src/routes/(3d)/threlte/Scene.svelte
+++ src/routes/(3d)/threlte/Scene.svelte
@@ -2,6 +2,14 @@
 	import { T } from "@threlte/core";
 </script>
 
+<T.PerspectiveCamera
+	makeDefault
+	position={[10, 10, 10]}
+	oncreate={(ref) => {
+		ref.lookAt(0, 1, 0);
+	}}
+/>
+
 <T.Mesh position.y={1}>
 	<T.BoxGeometry args={[2.5, 3.5, 1]} />
 	<T.MeshBasicMaterial color="hotpink" />
