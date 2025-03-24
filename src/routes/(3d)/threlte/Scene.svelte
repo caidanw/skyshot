e60e51504252fdd0143387f011e0e699a19d9288
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-03-24 01:03:06 -0700

    enh: add rounded box and orbit controls

--- src/routes/(3d)/threlte/Scene.svelte
+++ src/routes/(3d)/threlte/Scene.svelte
@@ -1,6 +1,11 @@
 <script>
 	import { T, useTask } from "@threlte/core";
-	import { interactivity, useTexture } from "@threlte/extras";
+	import {
+		interactivity,
+		OrbitControls,
+		RoundedBoxGeometry,
+		useTexture,
+	} from "@threlte/extras";
 	import { Spring } from "svelte/motion";
 
 	interactivity();
@@ -10,37 +15,37 @@
 
 	const scale = new Spring(2);
 
-	let rotation = 0;
-	useTask((delta) => {
-		rotation += delta;
-	});
+	// let rotation = 0;
+	// useTask((delta) => {
+	// 	rotation += delta;
+	// });
 
-	const idleTask = useTask("idle", (delta) => {
-		// wobble-effect
-		// bounce effect
-	});
+	// const idleTask = useTask("idle", (delta) => {
+	// 	// wobble-effect
+	// 	// bounce effect
+	// });
 </script>
 
 <T.PerspectiveCamera
 	makeDefault
-	fov={50}
-	position={[5, 5, 10]}
+	position={[10, 5, 10]}
 	oncreate={(ref) => {
 		ref.lookAt(0, 1, 0);
 	}}
-/>
+>
+	<OrbitControls enableDamping />
+</T.PerspectiveCamera>
 
 <T.AmbientLight intensity={0.5} />
 <T.DirectionalLight position={[0, 10, 10]} />
 
 <T.Mesh
-	rotation.y={rotation}
-	position.y={1}
 	scale={scale.current}
 	onpointerenter={() => {}}
 	onpointerleave={() => {}}
 >
-	<T.BoxGeometry args={[2.5, 3.5, 0.08]} />
+	<RoundedBoxGeometry args={[2.5, 3.5, 0.08]} creaseAngle={0.8} />
+	<!-- <T.BoxGeometry args={[2.5, 3.5, 0.08]} /> -->
 
 	{#await useTexture(imageTextureUrl)}
 		<T.MeshStandardMaterial color="black" />
