2c68e85f2d7dc96a61156ec2717ae5a3fcfad814
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-03-26 15:19:04 -0700

    ref: move footer to own component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Separator } from "./ui/separator";
+</script>
+
+<Separator />
+<footer
+	class="container flex flex-col items-center justify-center py-2 leading-5"
+>
+	<p>
+		<span>a creation from</span>
+		<a
+			class="font-medium text-sky-500"
+			href="https://bsky.app/profile/caidan.dev"
+			target="_blank"
+		>
+			<span>@caidan.dev</span>
+		</a>
+	</p>
+	<p>
+		<span>open source on</span>
+		<a
+			class="font-medium text-sky-500"
+			href="https://github.com/caidanw/skyshot"
+			target="_blank"
+		>
+			<span>github</span>
+		</a>
+	</p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,30 +22,4 @@
 	<Separator />
 
 	<slot />
-
-	<Separator />
-	<footer
-		class="container flex flex-col items-center justify-center py-2 leading-5"
-	>
-		<p>
-			<span>a creation from</span>
-			<a
-				class="font-medium text-sky-500"
-				href="https://bsky.app/profile/caidan.dev"
-				target="_blank"
-			>
-				<span>@caidan.dev</span>
-			</a>
-		</p>
-		<p>
-			<span>open source on</span>
-			<a
-				class="font-medium text-sky-500"
-				href="https://github.com/caidanw/skyshot"
-				target="_blank"
-			>
-				<span>github</span>
-			</a>
-		</p>
-	</footer>
 </div>
