6a564bda129c17643d73e532c5b398e7248e26c0
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-03-27 14:30:01 -0700

    enh: add canvas page for exploring image generation

---
+++ src/routes/canvas/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	let canvas: HTMLCanvasElement;
+
+	const width = 500;
+	const height = 700;
+
+	function drawBorder(
+		ctx: CanvasRenderingContext2D,
+		borderWidth: number = 0,
+	) {
+		if (borderWidth <= 0) {
+			return;
+		}
+
+		ctx.lineWidth = borderWidth;
+		ctx.rect(
+			borderWidth / 2,
+			borderWidth / 2,
+			width - borderWidth,
+			height - borderWidth,
+		);
+		ctx.stroke();
+	}
+
+	$effect(() => {
+		const ctx = canvas.getContext("2d");
+		if (!ctx) {
+			throw new Error("Failed to get 2D context from canvas");
+		}
+
+		// Draw border
+		ctx.strokeStyle = "red";
+		drawBorder(ctx, 1);
+	});
+</script>
+
+<main class="flex-grow">
+	<canvas bind:this={canvas} class="border border-black" {width} {height}>
+	</canvas>
+</main>
