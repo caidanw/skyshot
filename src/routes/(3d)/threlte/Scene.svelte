0f1d0f03fb46ac38db2f0586fc75da61a2840ada
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-03-28 13:42:06 -0700

    enh: update Scene to dynamically render card texture based on canvas context

--- src/routes/(3d)/threlte/Scene.svelte
+++ src/routes/(3d)/threlte/Scene.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { T, useTask } from "@threlte/core";
-	import { interactivity, OrbitControls, useTexture } from "@threlte/extras";
+	import { interactivity, OrbitControls } from "@threlte/extras";
 	import type { Mesh } from "three";
+	import { createCardCanvasContext, drawBorder } from "../../canvas/utils";
+	import { onMount } from "svelte";
+	import { Pane } from "tweakpane";
 
 	interactivity();
 
@@ -61,6 +64,67 @@
 	// 	// wobble-effect
 	// 	// bounce effect
 	// });
+
+	const { ctx, texture } = createCardCanvasContext();
+
+	const borderStyle = $state({
+		width: 10,
+		offset: 0,
+		color: "#f00",
+	});
+
+	const img = new Image();
+	img.src = "https://placehold.co/500x700";
+	img.crossOrigin = "anonymous";
+	img.onload = render;
+
+	function render() {
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+		ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
+
+		// Draw border
+		drawBorder(
+			ctx,
+			borderStyle.width,
+			borderStyle.offset,
+			borderStyle.color,
+		);
+		texture.needsUpdate = true;
+	}
+	$effect(render);
+
+	onMount(() => {
+		const tweakpaneElem = document.getElementById("tweakpane");
+		if (!tweakpaneElem) {
+			console.error("target element for tweakpane not found");
+			return;
+		}
+
+		const pane = new Pane({
+			container: tweakpaneElem,
+			title: "Card Style",
+		});
+
+		const borderFolder = pane.addFolder({
+			title: "Border",
+		});
+		borderFolder.addBinding(borderStyle, "width", {
+			min: 0,
+			max: 50,
+			step: 1,
+		});
+		borderFolder.addBinding(borderStyle, "offset", {
+			min: 0,
+			max: 50,
+			step: 1,
+		});
+		borderFolder.addBinding(borderStyle, "color");
+
+		return () => {
+			pane.dispose();
+		};
+	});
 </script>
 
 <T.PerspectiveCamera makeDefault position={[0, 0, 10.5]}>
@@ -78,18 +142,30 @@
 	<!-- <RoundedBoxGeometry args={[2.5, 3.5, 0.08]} /> -->
 	<T.BoxGeometry args={[5, 7, 0.08]} />
 
-	{#await useTexture(imageTextureUrl)}
-		<T.MeshStandardMaterial color="black" />
-	{:then texture}
-		<T.MeshPhysicalMaterial
-			map={texture}
-			metalness={0.1}
-			roughness={1 - config.glossiness}
-			clearcoat={0.8}
-			clearcoatRoughness={0.2}
-			reflectivity={config.reflectivity * 0.9}
-			envMapIntensity={config.envMapIntensity * 0.8}
-			ior={1.4}
-		/>
-	{/await}
+	<!-- <T.MeshStandardMaterial map={texture} /> -->
+	<T.MeshPhysicalMaterial
+		map={texture}
+		metalness={0.1}
+		roughness={1 - config.glossiness}
+		clearcoat={0.8}
+		clearcoatRoughness={0.2}
+		reflectivity={config.reflectivity * 0.9}
+		envMapIntensity={config.envMapIntensity * 0.8}
+		ior={1.4}
+	/>
+
+	<!-- {#await useTexture(imageTextureUrl)} -->
+	<!-- 	<T.MeshStandardMaterial color="black" /> -->
+	<!-- {:then texture} -->
+	<!-- 	<T.MeshPhysicalMaterial -->
+	<!-- 		map={texture} -->
+	<!-- 		metalness={0.1} -->
+	<!-- 		roughness={1 - config.glossiness} -->
+	<!-- 		clearcoat={0.8} -->
+	<!-- 		clearcoatRoughness={0.2} -->
+	<!-- 		reflectivity={config.reflectivity * 0.9} -->
+	<!-- 		envMapIntensity={config.envMapIntensity * 0.8} -->
+	<!-- 		ior={1.4} -->
+	<!-- 	/> -->
+	<!-- {/await} -->
 </T.Mesh>
